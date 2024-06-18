3335445ae43c2a7c6b9f16112e6e77fd58ff9500
Aarushi <aarushigoel.ag05@gmail.com>
2024-06-18 15:07:20 +0530

    UserInputSumDifference.cpp
    A CPP program, that takes 2 integer numbers as input from user, calculates sum and difference , and prints the results.

---
+++ UserInputSumDifference.cpp
@@ -0,0 +1,15 @@
+/*A CPP program, that takes 2 integer numbers as input from user, calculates sum and difference , and prints the results.*/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int a,b;
+	cout << "Enter 2 numbers :- " << endl;
+	cin >> a >> b;
+	cout << "Sum of the two numbers is : " << (a+b) << endl;
+	cout << "Difference between the two numbers is : " << (a-b) << endl;
+	return(0);
+}
+
